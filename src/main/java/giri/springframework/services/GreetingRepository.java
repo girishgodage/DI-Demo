5a79d2a47bd671f03c35d0fb210b5700a6009977
girishgodage <girishgodage@gmail.com>
2018-07-13 11:06:51 +0530

    Added Repoditory and jave Config and create factory

---
+++ src/main/java/giri/springframework/services/GreetingRepository.java
@@ -0,0 +1,10 @@
+package giri.springframework.services;
+
+public interface GreetingRepository {
+
+    String getEnglishGreeting();
+
+    String getSpanishGreeting();
+
+    String getGermanGreeting();
+}
